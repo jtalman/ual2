4c81c505a252c5a39fbb546db7d027be01eb426e
John Talman <JohnDTalman@gmail.com>
2025-05-29 13:01:05 -0400

    $UAL1/tools pushed under legacy

--- tools/sxf/src/echo_sxf/EchoAcceleratorReader.cc
+++
@@ -1,9 +0,0 @@
-#include "echo_sxf/EchoAcceleratorReader.hh"
-#include "echo_sxf/EchoNodeRegistry.hh"
-
-// Constructor.
-SXF::EchoAcceleratorReader::EchoAcceleratorReader(SXF::OStream& out)
-  : SXF::AcceleratorReader(out)
-{
-  m_pNodeRegistry = SXF::EchoNodeRegistry::getInstance(out);
-}
